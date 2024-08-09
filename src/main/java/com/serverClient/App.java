c60fb313c969bd11d58d41be3b251e9fc5a05b78
Conner <conneryule@gmail.com>
2024-08-09 11:23:37 -0400

    start

---
+++ src/main/java/com/serverClient/App.java
@@ -0,0 +1,13 @@
+package com.serverClient;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
